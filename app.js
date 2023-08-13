3de42dafa4bb10826f8bce7a1911eb4cc7722f88
Anvq2003 <97084643+Anvq2003@users.noreply.github.com>
2023-08-13 11:03:08 +0700

    Edit app

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const serviceAccount = require('./zingmp3_key.json');
+
+const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: privateKey,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
 const { connect } = require('./config/db.config');
 const routes = require('./routes');
